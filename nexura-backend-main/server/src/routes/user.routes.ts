74d3ac5c351b1f0dfbe41d94f5bf1d9bac0cd763
cakezero <sareahj.com@gmail.com>
2025-11-26 01:31:40 +0100

    updated routes to contain leaderboard

--- nexura-backend-main/server/src/routes/user.routes.ts
+++ nexura-backend-main/server/src/routes/user.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import { fetchUser, updateUsername } from "@/controllers/app.controller";
-import { claimDailyQuest, claimOneTimeQuest } from "@/controllers/tasks.controller";
+import { claimQuest } from "@/controllers/tasks.controller";
 import { signUp } from "@/controllers/auth.controller";
 import { authenticateUser } from "@/middlewares/auth.middleware";
 
 const router = Router();
 
 router
-	.post("/claim-daily-quest", authenticateUser, claimDailyQuest)
-	.post("/claim-one-time-quest", authenticateUser, claimOneTimeQuest)
+	.post("/claim-quest", authenticateUser, claimQuest)
 	.get("/profile", authenticateUser, fetchUser)
 	.post("/sign-up", signUp)
 	.patch("/update", authenticateUser, updateUsername);
